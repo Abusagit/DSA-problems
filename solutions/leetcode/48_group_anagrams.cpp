d94c4d85de9c6e08d486223ea3c53c3aeebb376b
abusagit <mirotvorez00@gmail.com>
2022-10-01 15:58:48 +0300

    feat add group_anagrams.cpp

---
+++ solutions/leetcode/48_group_anagrams.cpp
@@ -0,0 +1,50 @@
+// Example:
+// Input: strs = ["eat", "tea","tan","ate","nat","bat"]
+// Output: [["bat"], ["nat","tan"],["ate","eat","tea"]]
+
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <iterator>
+
+
+class Solution {
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        std::unordered_map<std::string, std::vector<std::string>> map;
+
+        for (std::string x: strs){
+            std::vector<int> encoding(26, 0);
+
+            for (std::string::iterator c=x.begin(); c != x.end(); c++)
+            {
+                encoding[*c - 'a']++;
+            }
+
+
+            // vector<int> -> string
+            std::stringstream result;
+            std::copy(encoding.begin(), encoding.end(), std::ostream_iterator<int>(result, "-"));
+            std::string s = result.str();
+
+            if (map.find(s) == map.end()){
+                map[s] = {x};
+            } else {
+                map[s].push_back(x);
+            }
+
+
+        }
+
+        std::vector<std::vector<std::string>> result;
+
+        for (auto key_value: map){
+            result.push_back(key_value.second);
+
+        }
+
+        return result;
+    }
+};
