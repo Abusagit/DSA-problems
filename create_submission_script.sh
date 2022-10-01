795df743ea246e412aa414f9aa1d2b2c19552b11
abusagit <mirotvorez00@gmail.com>
2022-10-01 21:25:51 +0300

    feat add automatic_solution_script for creation

---
+++ create_submission_script.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+usage="$(basename "$0") - Template for automatic creation of raw, blank scripts with given name and extension in provided directory - because who wants to type it by its own???!
+        -d <dir> [current_dir] - directory for new file to be saved at
+        -n <Any name> - name of a file in qoutes - any dots will be removed and spaces replaces by underscores
+        -e <extension> [cpp] - extension of a future file
+
+    Enjoy!
+"
+
+EXTENSION="cpp"
+OUTDIR="./"
+
+if [ "$1" == "-h" ]; then
+  echo "$usage"
+  exit 0
+fi
+
+while getopts ":d:n:h:e:" opt; do
+    case $opt in
+        d)
+            OUTDIR=$OPTARG;;
+        n) 
+            NAME="$OPTARG";;
+
+        e) 
+            EXTENSION="$OPTARG";;
+
+        \?)
+            echo "Invalid parameter!"
+            exit;;
+    esac
+done
+
+
+NAME="${NAME// /_}" # replace spaces by underscores
+NAME="${NAME//./}.${EXTENSION}" # delete dots from name
+
+mkdir -p $OUTDIR # create directory if it doesn`t exist
+
+
+whole_name="${OUTDIR}/${NAME}"
+touch $whole_name
+echo "File with name ${NAME} was created at ${whole_name}"
