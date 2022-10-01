8b43bc9f8bedf5dbf4dab224cd2af9779f862872
abusagit <mirotvorez00@gmail.com>
2022-10-01 20:49:14 +0300

    feat add another solution to 128_leetcode

--- solutions/leetcode/128_Longest_Consecutive_Sequence.cpp
+++ solutions/leetcode/128_Longest_Consecutive_Sequence.cpp
@@ -3,13 +3,17 @@
 #include <algorithm>
 
 
-using std::max;
+
+
 
 class Solution {
+using std::vector;
+using std::unordered_set;
+using std::max;
+
 public:
     int longestConsecutive(vector<int>& nums) {
-        using std::vector;
-        using std::unordered_set;
+
         
         unordered_set<int> set;
 
@@ -46,6 +50,35 @@ public:
         
             longest_consequtive_sequence = max({longest_consequtive_sequence, longest_consequtive_sequence_for_x});
         }
+
+        return longest_consequtive_sequence;
+
+    }
+
+
+    int longestConsecutive_onerun(vector<int>& nums){
+        unordered_set<int> set(nums.begin(), nums.end());
+
+        int longest_consequtive_sequence = 0;
+
+        for (auto & n: set){
+
+            if (!set.count(n - 1)){ //if n is the smallest element - start counting
+                int length_of_sequence = 1;
+
+                while(set.count(n+length_of_sequence) != 0){
+                    length_of_sequence++;
+
+                }
+
+                longest_consequtive_sequence = max({longest_consequtive_sequence, length_of_sequence});
+
+
+
+            }
+        
+        }
+
 
         return longest_consequtive_sequence;
 
