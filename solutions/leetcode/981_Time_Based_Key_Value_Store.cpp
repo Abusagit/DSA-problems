b8fdc339f566d95163d5eb647f6068fa5c138f9d
abusagit <mirotvorez00@gmail.com>
2022-10-22 15:39:25 +0300

    update time-based key-value

--- solutions/leetcode/981_Time_Based_Key_Value_Store.cpp
+++ solutions/leetcode/981_Time_Based_Key_Value_Store.cpp
@@ -16,44 +16,42 @@ public:
     }
     
     void set(string key, string value, int timestamp) {
-        if (timestamp_values.find(key) != timestamp_values.end()){
-            timestamp_values[key].push_back({timestamp, value});
-        } else {
-            timestamp_values[key] = {{0, ""}, {timestamp, value}};
-        }
+        timestamp_values[key].push_back({timestamp, value});
     }
     
     string get(string key, int timestamp) {
+        
+        if (timestamp_values.find(key) == timestamp_values.end()){
+            return "";
+        }
+        
+        int left = 0;
+        int right = timestamp_values[key].size() - 1;
 
-        vector<pair<int, string>> query_vector = timestamp_values[key];
+        while (left <= right){
+            int middle = right + (left - right) / 2;
 
-        string rightmost_s = binary_search_of_the_rightmost_value(query_vector, timestamp);
-        return rightmost_s;
+            int middle_t = timestamp_values[key][middle].first;
+
+            if (timestamp < middle_t){
+                right = middle - 1;
+            } else if (timestamp > middle_t){
+                left = middle + 1;
+            } else {
+                return timestamp_values[key][middle].second;
+            }
+
+        }
+        
+        if (right >= 0){
+            return timestamp_values[key][right].second;
+        }
+        return "";
 
     }
 
 private:
     unordered_map<string, vector<pair<int, string>>> timestamp_values;
-
-    string binary_search_of_the_rightmost_value(vector<pair<int, string>>& timestamps, int time){
-        int left = 0;
-        int right = timestamps.size();
-
-        while (left < right){
-            int middle = right + (left - right) / 2;
-
-            int middle_t = timestamps[middle].first;
-
-            if (time < middle_t){
-                right = middle;
-            } else {
-                left = middle + 1;
-            }
-
-        }
-
-        return timestamps[left].second;
-    }
 };
 
 /**
