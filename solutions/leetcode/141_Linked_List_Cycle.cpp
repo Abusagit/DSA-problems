73fc423cf07b308f9e43f5044cd5d1b2b616d1e8
abusagit <mirotvorez00@gmail.com>
2022-11-08 22:58:45 +0300

    feat add faster solution

--- solutions/leetcode/141_Linked_List_Cycle.cpp
+++ solutions/leetcode/141_Linked_List_Cycle.cpp
@@ -28,4 +28,26 @@ public:
 
         return false;
     }
+
+    bool hasCycle_no_memory(ListNode *head){
+
+        //on a signle iteration the distance between fast and slow pointer is reduced by 1 ==> time for fast pointer to reach the slow pointer is O(n)
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast != nullptr && fast->next != nullptr){
+
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (fast == slow){ // check if they intersect
+                return true;
+            }
+
+        }
+
+        return false;
+    }
 };
+
