0bad3dc3a9260fe54c2beeb9e05c065309ed8207
abusagit <mirotvorez00@gmail.com>
2022-12-04 00:10:45 +0300

    feat add 7- climbing stairs

---
+++ solutions/leetcode/70_Climbing_Stairs.cpp
@@ -0,0 +1,17 @@
+// just Fibbonaci numbers
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int d[n + 1];
+        d[0] = 1;
+        d[1] = 1;
+        
+        for (int i = 2; i <= n; i++){
+            
+            d[i] = d[i - 1] + d[i - 2];
+        }
+        
+        return d[n];
+    }
+};
