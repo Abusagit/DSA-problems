1c5787a796062e62d70ac93894237be0ff722db5
abusagit <mirotvorez00@gmail.com>
2022-12-26 22:15:19 +0300

    feat add non-recursive and faster solutions

--- solutions/leetcode/226_Invert_Binary_Tree.cpp
+++ solutions/leetcode/226_Invert_Binary_Tree.cpp
@@ -1,14 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -16,6 +18,33 @@ public:
 
         return root;
     }
+
+    TreeNode* invertTreeQuick(TreeNode* root){
+        if (root == nullptr){
+            return nullptr;    
+        }
+
+        swap(root->left, root->right);
+        invertTreeQuick(root->left);
+        invertTreeQuick(root->right);
+        return root;
+    }
+
+    TreeNode* invertTreeStack(TreeNode* root) {
+    std::stack<TreeNode*> stk;
+    stk.push(root);
+    
+    while (!stk.empty()) {
+        TreeNode* p = stk.top();
+        stk.pop();
+        if (p) {
+            stk.push(p->left);
+            stk.push(p->right);
+            std::swap(p->left, p->right);
+        }
+    }
+    return root;
+}
 
 private:
     void invertNode(TreeNode* node){
