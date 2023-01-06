1a6f9ff6396f16502ef463552f16b584a596a9a7
abusagit <mirotvorez00@gmail.com>
2023-01-06 18:10:10 +0300

    feat add combination sum

---
+++ solutions/leetcode/40_Combination_Sum_II.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+
+        vector<int> current;
+
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, result, current, 0, 0, target);
+        return result;
+    }
+private:
+    void dfs(vector<int>& candidates, vector<vector<int>>& result, vector<int>& current, int sum, int start_index, int& target){
+        if (sum > target){
+            return;
+        }
+        
+        if (sum == target){
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = start_index; i < candidates.size(); i++){
+            if (i > start_index && candidates[i] == candidates[i - 1]){
+                continue;
+            }
+            current.push_back(candidates[i]);
+            dfs(candidates, result, current, sum + candidates[i], i + 1, target);
+            current.pop_back();
+        }
+
+        
+    }
+
+};
