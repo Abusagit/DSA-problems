8b4d0d1f0d514223bb9d12919f53de33e703d44f
abusagit <mirotvorez00@gmail.com>
2023-01-07 13:12:07 +0300

    update pow

--- solutions/leetcode/50_Pow_x_n.cpp
+++ solutions/leetcode/50_Pow_x_n.cpp
@@ -9,6 +9,14 @@ public:
         // n = 0 is prevented implicitly
 
         // x ^ (2a) = (x ^ 2) ^ a
+        // x ^ (2a + 1) = x * (x ^ 2) ^ a
+        
+
+
+        // at each i we compute result * pow(current_value, exponent)
+
+        // if exponent is odd --> result * current_value and then compute pow(current_valut ^ 2, exponent / 2)
+        // otherwise compute 1 * pow(current_value ^ 2, exponent / 2)
         for (long i = exponent; i > 0; i /= 2){
             if (i % 2){
                 result *= current_value;
