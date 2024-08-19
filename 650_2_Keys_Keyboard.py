db8c895fe1ef604d29aad1a02a7850defd48c3d0
Fyodor Velikonivtsev <mirotvorez00@gmail.com>
2024-08-20 00:55:56 +0300

    add prime factoring to 650

--- 650_2_Keys_Keyboard.py
+++ 650_2_Keys_Keyboard.py
@@ -1,6 +1,6 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-
+        """O(n)"""
         result = 0
 
         for divisor in range(2, n+1):
@@ -9,3 +9,22 @@ class Solution:
                 result += divisor
 
         return result
+
+    def minSteps(self, n: int) -> int:
+        """O(logn)"""
+        if n == 1:
+            return 0
+
+        result = 0
+
+        divisor = 2
+
+        while divisor * divisor <= n:
+            while n % divisor == 0:
+                n //= divisor
+                result += divisor
+
+            divisor += 1
+        
+
+        return result + n if n > 1 else result # if n > 1 then n is Prime => add n
